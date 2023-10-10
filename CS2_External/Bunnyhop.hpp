77ee5374ed04770116d1edbbbcc6b1e858296758
Random <42265006+OvertimeDev-Random@users.noreply.github.com>
2023-10-10 15:28:37 -0500

    Bhop Fix

--- CS2_External/Bunnyhop.hpp
+++ CS2_External/Bunnyhop.hpp
@@ -6,7 +6,7 @@ namespace Bunnyhop
 {
 	inline void Run(const CEntity& Local)
 	{
-
+		const bool hasFlagInAir = Local.Pawn.HasFlag(PlayerPawn::Flags::IN_AIR);
 		if (GetAsyncKeyState(VK_SPACE) && Local.Pawn.fFlags)
 		{
 			gGame.SetForceJump(65537);
