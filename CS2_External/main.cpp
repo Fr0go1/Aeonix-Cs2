65ed157154f684abd2d892ab70ef04f3d42b50d4
Fr0go1 <112802612+Fr0go1@users.noreply.github.com>
2023-10-12 07:58:00 +1000

    updated to cpp 20

--- CS2_External/main.cpp
+++ CS2_External/main.cpp
@@ -2,45 +2,10 @@
 #include "Cheats.h"
 #include <iostream>
 #include <iomanip>
-#include <Shlobj.h>
-#include <filesystem>
-#include <Windows.h>
-
-#define RESET   "\033[0m"
-#define RED     "\033[31m"
-#define GREEN   "\033[32m"
-#define YELLOW  "\033[33m"
-#define BLUE    "\033[34m"
+#include <format>
 
 int main()
 {
-
-	TCHAR documentsPath[MAX_PATH];
-	if (SHGetFolderPath(NULL, CSIDL_MYDOCUMENTS, NULL, 0, documentsPath) != S_OK) {
-		return 0;
-	}
-
-	char narrowPath[MAX_PATH];
-	if (WideCharToMultiByte(CP_UTF8, 0, documentsPath, -1, narrowPath, sizeof(narrowPath), NULL, NULL) == 0) {
-		return 0;
-	}
-
-	std::string documentsDir(narrowPath);
-	std::string configFilePath = documentsDir + "\\.Aeonix\\";
-
-	namespace fs = std::filesystem;
-	if (!fs::is_directory(configFilePath)) {
-		if (fs::create_directory(configFilePath)) {
-			std::cout << "Configuration directory created: " << configFilePath << std::endl;
-		}
-		else {
-			std::cerr << "Error: Could not create the configuration directory." << std::endl;
-		}
-	}
-	else {
-		std::cout << "Configuration directory already exists: " << configFilePath << std::endl;
-	}
-
 	auto ProcessStatus = ProcessMgr.Attach("cs2.exe");
 	if (ProcessStatus != StatusCode::SUCCEED)
 	{
@@ -56,21 +21,22 @@ int main()
 
 	if (!gGame.InitAddress())
 	{
-		std::cout << "[ERROR] Failed to call InitAddress()."<< std::endl;
+		std::cout << "[ERROR] Failed to call InitAddress()." << std::endl;
 		goto END;
 	}
 
-	std::cout << "[Pid]:" << ProcessMgr.ProcessID << std::endl;
-	std::cout << "[Client]:" << gGame.GetClientDLLAddress() << std::endl;
+	std::cout << std::format("[Pid]:{}\n", ProcessMgr.ProcessID);
+	std::cout << std::format("[Client]:{:X}\n", gGame.GetClientDLLAddress());
 
-	std::cout << "[EntityList]:" << std::setiosflags(std::ios::uppercase) << std::hex << Offset::EntityList << std::endl;
-	std::cout << "[Matrix]:" << std::setiosflags(std::ios::uppercase) << std::hex << Offset::Matrix << std::endl;
-	std::cout << "[LocalPlayerController]:" << std::setiosflags(std::ios::uppercase) << std::hex << Offset::LocalPlayerController << std::endl;
-	std::cout << "[ViewAngles]:" << std::setiosflags(std::ios::uppercase) << std::hex << Offset::ViewAngle << std::endl;
-	std::cout << "[LocalPlayerPawn]:" << std::setiosflags(std::ios::uppercase) << std::hex << Offset::LocalPlayerPawn << std::endl;
-	std::cout << "[ForceJump]:" << std::setiosflags(std::ios::uppercase) << std::hex << Offset::ForceJump << std::endl;
+	std::cout << "Offset:" << std::endl;
+	std::cout << std::format("[EntityList]:{:X}\n", Offset::EntityList);
+	std::cout << std::format("[Matrix]:{:X}\n", Offset::Matrix);
+	std::cout << std::format("[LocalPlayerController]:{:X}\n", Offset::LocalPlayerController);
+	std::cout << std::format("[ViewAngles]:{:X}\n", Offset::ViewAngle);
+	std::cout << std::format("[LocalPlayerPawn]:{:X}\n", Offset::LocalPlayerPawn);
+	std::cout << std::format("[ForceJump]:{:X}\n", Offset::ForceJump);
 
-	std::cout << "Running..." << std::endl;
+	std::cout << "Runing..." << std::endl;
 
 	try
 	{
