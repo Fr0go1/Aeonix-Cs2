1db2a3e6bdf0fbfa68c71489990dcfa6e92e4701
Fr0go1 <112802612+Fr0go1@users.noreply.github.com>
2023-10-20 09:08:47 +1000

    Added random triggerdelay comments
    im at school so i will make sure it works once im home

--- CS2_External/TriggerBot.hpp
+++ CS2_External/TriggerBot.hpp
@@ -3,12 +3,19 @@
 #include "Entity.h"
 #include "MenuConfig.hpp"
 #include <chrono>
+//#include <random>
 
 namespace TriggerBot
 {
+	//inline DWORD MinDelay = 50; // Minimum delay in milliseconds
+	//inline DWORD MaxDelay = 150; // Maximum delay in milliseconds
 	inline DWORD TriggerDelay = 90; // ms
 	inline int HotKey = VK_LMENU;
 	inline std::vector<int> HotKeyList{ VK_LMENU, VK_RBUTTON, VK_XBUTTON1, VK_XBUTTON2, VK_CAPITAL, VK_LSHIFT, VK_LCONTROL };
+
+	std::random_device rd;
+	std::mt19937 gen(rd());
+	std::uniform_int_distribution<DWORD> delayDistribution(MinDelay, MaxDelay);
 
 	inline void SetHotKey(int Index)
 	{
@@ -17,6 +24,8 @@ namespace TriggerBot
 
 	inline void Run(const CEntity& LocalEntity)
 	{
+		//DWORD RandomTriggerDelay = delayDistribution(gen);
+		
 		DWORD uHandle = 0;
 		if (!ProcessMgr.ReadMemory<DWORD>(LocalEntity.Pawn.Address + Offset::Pawn.iIDEntIndex, uHandle))
 			return;
@@ -48,6 +57,7 @@ namespace TriggerBot
 
 		static std::chrono::time_point LastTimePoint = std::chrono::steady_clock::now();
 		auto CurTimePoint = std::chrono::steady_clock::now();
+		//if (CurTimePoint - LastTimePoint >= std::chrono::milliseconds(RandomTriggerDelay))
 		if (CurTimePoint - LastTimePoint >= std::chrono::milliseconds(TriggerDelay))
 		{
 			const bool isAlreadyShooting = GetAsyncKeyState(VK_LBUTTON) < 0;
